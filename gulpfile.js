3317a8b201b08ee9be855eecccd6af8ca84faa4c
ui.sawada <ui.sawada@webcrew.co.jp>
2018-08-10 17:35:29 +0900

    add すでに作成済ファイル追加

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+//プラグイン
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//タスク
+gulp.task('default', function () {
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css/'));
+});
